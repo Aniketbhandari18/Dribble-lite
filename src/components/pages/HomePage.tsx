045c326eb8eb4f4c0e3fa2e3f06ddf675aa792aa
Aniket Bhandari <aniketbhandari1807@gmail.com>
2025-09-07 22:31:50 +0530

    fix: escape single quotes in JSX with &apos;

--- src/components/pages/HomePage.tsx
+++ src/components/pages/HomePage.tsx
@@ -5,7 +5,7 @@ export default function HomePage() {
     <div className="pt-6 p-6">
       <main className="flex flex-col items-center justify-center space-y-4 mb-14">
         <h1 className="text-2xl xs:text-3xl sm:text-5xl font-sans font-bold flex flex-col items-center justify-center">
-          <p>Discover the World's</p>
+          <p>Discover the World&apos;s</p>
           <p>Top Designs</p>
         </h1>
         <p className="max-w-110 text-center text-md font-semibold leading-5 text-gray-600">

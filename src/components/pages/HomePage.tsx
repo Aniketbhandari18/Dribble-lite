d3f1a7251351d44030017a741b62ea18d8025abd
Aniket Bhandari <aniketbhandari1807@gmail.com>
2025-09-07 14:49:14 +0530

    add homepage

---
+++ src/components/pages/HomePage.tsx
@@ -0,0 +1,16 @@
+export default function HomePage() {
+  return (
+    <div className="pt-6">
+      <main className="flex flex-col items-center justify-center space-y-4">
+        <h1 className="text-2xl xs:text-3xl sm:text-5xl font-sans font-bold flex flex-col items-center justify-center">
+          <p>Discover the World's</p>
+          <p>Top Designs</p>
+        </h1>
+        <p className="max-w-110 text-center text-md font-semibold leading-5 text-gray-600">
+          Explore work from the most talented and accomplished designers ready
+          to take on your next project.
+        </p>
+      </main>
+    </div>
+  );
+}

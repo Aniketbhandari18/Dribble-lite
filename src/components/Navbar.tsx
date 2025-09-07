ba7387d3bb570c8d2965298df4a7687a95b84bc2
Aniket Bhandari <aniketbhandari1807@gmail.com>
2025-09-07 22:50:09 +0530

    remove searchbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -35,7 +35,7 @@ export default function Navbar() {
           </span>
         </Link>
 
-        <Searchbar />
+        {/* <Searchbar /> */}
 
         <div className="flex items-center space-x-4">
           <SignedIn>
